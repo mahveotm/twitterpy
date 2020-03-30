acedead6a5a57d08f6198756373bfdc1b3c91e97
mahveo <mahveotm@gmail.com>
2020-03-30 23:16:14 +0100

    that didn't work.

--- Viewstatusupdate.py
+++ Viewstatusupdate.py
@@ -6,6 +6,11 @@ import tweepy, time
 #ACCESS_KEY =os.environ['access_key']
 #ACCESS_SECRET =os.environ['access_secret']
 
+CONSUMER_KEY = '$consumer_key'
+CONSUMER_SECRET ='$consumer_secret'
+ACCESS_KEY ='$access_key'
+ACCESS_SECRET ='$access_secret'
+
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
